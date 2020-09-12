f0571aacdee44e162c848e30c506d3e1b018355c
VaradVartak57 <71070930+VaradVartak57@users.noreply.github.com>
2020-09-12 14:35:24 +0530

    Add files via upload

---
+++ commandadd.java
@@ -0,0 +1,14 @@
+public class commandadd {
+    public static void main(String args[]) {
+        int sum = 0;
+        for(int i=0;i<args.length;i++)
+
+        {
+                    sum=sum + Integer.parseInt(args[i]);
+            
+        }
+         System.out.println("The result is:"+sum);
+    }
+}
+    
+

590b0558b42fc4617554e247cb4f7d78188fe426
VaradVartak57 <71070930+VaradVartak57@users.noreply.github.com>
2020-11-23 09:06:01 +0530

    Add files via upload

--- string1.java
+++ string1.java
@@ -1,13 +1,16 @@
+import java.util.*;
 public class string1 {
     public static void main (String args[])
     {
-        String name="hello World how ARE yOU";
+        Scanner inp = new Scanner(System.in);
+		System.out.print("Enter string name: ");
+        String name = inp.nextLine();
         System.out.println("Name: " + name);
         System.out.println("Uppercase: " + name.toUpperCase());
         System.out.println("Lowercase: " + name.toLowerCase());
         System.out.println("First char: " + name.charAt(0));
         System.out.println("Length: " + name.length());
-        System.out.println("last char: " + name.charAt(22));
-        System.out.println("Substring: " + name.substring(12, 23));
+        System.out.println("last char: " + name.charAt(17));
+        System.out.println("Substring: " + name.substring(5,10 ));
     }
 }
